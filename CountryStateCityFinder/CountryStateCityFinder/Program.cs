0d3c5500d34d39791a040ddf72fe759e55210bf4
MAGENIC\JuliusN <juliusn@magenic.com>
2021-06-05 19:34:01 +0800

    initial commit

---
+++ CountryStateCityFinder/CountryStateCityFinder/Program.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+
+namespace CountryStateCityFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Program prog = new Program();
+            prog.GetData();
+
+        }
+
+
+        public void GetData()
+        {
+            if (File.Exists(@"C:\repos\Others\CountryStateCityFinder\CountryStateCityFinder\CountryStateCityFinder\countrystatecity.json"))
+            {
+                string jsontxt = File.ReadAllText(@"C:\repos\Others\CountryStateCityFinder\CountryStateCityFinder\CountryStateCityFinder\countrystatecity.json");
+                var countryList = JsonConvert.DeserializeObject<IEnumerable<Country>>(jsontxt);
+
+                var unitedStates = countryList.Where(c => c.Name == "United States");
+            }
+
+            
+        }
+
+
+
+    }
+
+    public class Country
+    {
+        public string Name { get; set; }
+        public IEnumerable<States> States { get; set; }
+    }
+
+    public class States
+    {
+        public string Name { get; set; }
+        public IEnumerable<Cities> Cities { get; set; }
+    }
+
+    public class Cities
+    {
+        public string Name { get; set; }
+    }
+
+
+}
